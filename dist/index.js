912359c4a71dc7020fbeaa15c2f5fd39b9bd141e
Ermek Galiev <ermekcs@gmail.com>
2022-12-12 15:02:23 +0600

    revert all changes

--- dist/index.js
+++ dist/index.js
@@ -17191,7 +17191,7 @@ async function exportSecrets() {
     });
 
     const results = await getSecrets(requests, client);
-    core.debug(requests);
+    core.debug(results);
 
     for (const result of results) {
         const { value, request, cachedResponse } = result;
